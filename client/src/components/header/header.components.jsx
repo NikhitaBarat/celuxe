a627194dc3f1d7f56e4ce43cfd3df2fb7b5e62a7
siddhantprateek <siddhantprateek@gmail.com>
2022-06-18 15:33:16 +0530

    header authentication added

--- client/src/components/header/header.components.jsx
+++ client/src/components/header/header.components.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./header.styles.css";
 import { useAuth0 } from "@auth0/auth0-react";
 function Header() {
-  const { user, loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin,
+    });
   return (
     <header>
       <div className="header-content">
@@ -16,7 +21,7 @@ function Header() {
         <div className="header-right">
           <ul className="header-option">
             <li>
-              <a href="/">Host</a>
+              <a href="/host">Host</a>
             </li>
             <li>
               <a href="/team">Team</a>
@@ -27,9 +32,13 @@ function Header() {
             <li>
               <a href="/contact">Contact</a>
             </li>
-            <li onClick={() => loginWithRedirect()}>
-              <a href="/login">Login</a>
-            </li>
+
+            {!isAuthenticated && (
+              <li onClick={() => loginWithRedirect()}>Login</li>
+            )}
+            {isAuthenticated && (
+              <li onClick={() => logoutWithRedirect()}>Logout</li>
+            )}
           </ul>
         </div>
       </div>
