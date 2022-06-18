4f1b81ea66fb64fc0a0792add479a083b72cdd7d
Nikhita <bnikhita2003@gmail.com>
2022-06-19 00:48:22 +0530

    depl v1

--- server.js
+++ server.js
@@ -32,14 +32,14 @@ app.use('/api/news', newsRoute)
 app.use('/graphql', graphRoute)
 
 
-// app.use(express.static(path.join(__dirname, "./client/build")))
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-// })
-
+app.use(express.static(path.join(__dirname, "./client/build")))
 app.get('/', (req, res) => {
-    res.send('Server route is working perfectly')
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
 })
+
+// app.get('/', (req, res) => {
+//     res.send('Server route is working perfectly')
+// })
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running at http://localhost:${PORT}/api`)
