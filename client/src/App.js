5fc020bb97872e624afa2a32d987bd862f9688f0
siddhantprateek <siddhantprateek@gmail.com>
2022-06-18 11:14:49 +0530

    Added React Router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/header.components";
+import { Header } from "./components";
+import { Home, About, Event, Find, News, Pantry, ScheduleEvent, Snap, Team, Host } from "./views";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="event" element={<Event />} />
+        <Route path="find" element={<Find />} />
+        <Route path="news" element={<News />} />
+        <Route path="pantryservices" element={<Pantry />} />
+        <Route path="scheduleevent" element={<ScheduleEvent />} />
+        <Route path="snap" element={<Snap />} />
+        <Route path="team" element={<Team />} />
+        <Route path="host" element={<Host />} />
+      </Routes>
     </div>
   );
 }
