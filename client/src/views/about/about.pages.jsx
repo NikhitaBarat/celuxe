b333c047368ce782a5e3c93709bea74adb857014
Nikhita <bnikhita2003@gmail.com>
2022-06-18 16:47:20 +0530

    added para to about page

--- client/src/views/about/about.pages.jsx
+++ client/src/views/about/about.pages.jsx
@@ -23,10 +23,15 @@ const About = () => {
         </div>
         <img src={Mid} className='about-mid' alt="" />
         <div className="mid-container">
-          <div className="container-border">
+          {/* <div className="container-border">
             <h1>HUNGER FREE CAMPUS</h1>
-          </div>
+          </div> */}
         </div>
+        <p className='about-para'>
+        In 2020 and 2021, we set out to explore students’ lived experiences of food insecurity. Along with 5 student co-researchers, trained in qualitative methods and with their own experience of food insecurity, we conducted 94 interviews with self-identified food insecure students attending 13 different higher education institutions across Victoria, Australia. Although many were international students, we had a diverse range of participants from those in completing their undergraduate degrees, graduates and PhD students. 
+        <br /><br />The research focused on understanding students’ relationship to food, both at home, on campus and around their neighbourhoods. We asked students about how food insecurity affected their friendships, peer and personal relationships, and their studies. We also found out about the interconnectedness of students’ food struggles with other areas of their lives including housing, finances and paid work. Although the research was planned prior to the pandemic, we quickly found that COVID-19 was a significant contributor to many students’ food woes, especially international students. Students employed a range of resourceful and creative tactics to cope with their everyday struggle. 
+        <br /><br />Finally, we made sure to ask students about how they thought we might begin to tackle the problem. These ideas formed the basis of our toolkit. 
+        </p>
       </div>
     </div>
   )
