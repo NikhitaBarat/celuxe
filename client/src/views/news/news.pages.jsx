1baee3b8e3414c8989b81c85c54a86b5c19118ae
siddhantprateek <siddhantprateek@gmail.com>
2022-06-18 21:33:53 +0530

    news page createad

--- client/src/views/news/news.pages.jsx
+++ client/src/views/news/news.pages.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { NewsBG } from "../../assets";
+import "./news.styles.css";
 const News = () => {
   return (
-    <div>News</div>
-  )
-}
+    <div className="news-page">
+      <div className="bg-relative">
+        <img className="news-bg" src={NewsBG} alt="" />
+        <div className="news-overlays"></div>
+        <div className="news-overlays"></div>
+      </div>
+      <div className="news-header-content">
+        <h1>
+          Covid spotlights hunger struggles for Australia’s overseas students
+        </h1>
+        <p>
+          Students collaborate to combat food shortages, as Covid widens the gap
+          between Australia’s haves and have-nots
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default News
+export default News;
