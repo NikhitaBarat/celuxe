c24c89cdf076969483ac61a75d2ecdfc5e8eb47b
Nikhita <bnikhita2003@gmail.com>
2022-06-19 12:56:02 +0530

    created nomatch

--- client/src/views/NoMatch/nomatch.pages.jsx
+++ client/src/views/NoMatch/nomatch.pages.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './nomtach.style.css'
+import { NotFoundBg } from '../../assets'
+
 const Nomatch = () => {
   return (
     <div className='no-match-page'>
-        <h1>No Match</h1>
+      <div className="event-bg">
+        <img className="host-event-bg" src={NotFoundBg} alt="" />
+        <div className="overlay-bg"></div>
+        <div className="overlay-bg"></div>
+      </div>
+      <div className="nf-text">
+        <h1>404</h1>
+        <p>Either you aren't cool enough to visit this page or it doesn't exist like your social life.</p>
+      </div>
     </div>
   )
 }
