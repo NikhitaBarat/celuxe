eb3ab78afb879807dc09a62df113d6725ceaa62d
siddhantprateek <siddhantprateek@gmail.com>
2022-06-19 08:50:44 +0530

    apollo graphql added to client

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import history from "./utils/history";
 import { getConfig } from "./config";
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider 
+} from '@apollo/client'
 
 const onRedirectCallback = (appState) => {
   history.push(
@@ -24,12 +29,23 @@ const providerConfig = {
   onRedirectCallback,
 };
 
+// production uri: 
+// development uri: http://localhost:4000/graphql
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0Provider {...providerConfig}>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
